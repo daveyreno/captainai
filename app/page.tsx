6969c682583ee2a12cac997bd2cb77e422db5944
Dave Reno <daveyreno86@gmail.com>
2023-04-26 15:59:29 +0800

    Menu Updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className=''>
-      <div className='flex flex-col h-screen justify-center items-center'>
-        <h1 className='text-8xl tracking-tighter font-extrabold mb-4'>CaptainAI</h1>
-        <p className='text-2xl mb-8 text-slate-500'>Improving productivity and certainty.</p>
+      <div className='flex flex-col items-center justify-center h-screen'>
+        <h1 className='mb-4 font-extrabold tracking-tighter text-8xl'>CaptainAI</h1>
+        <p className='mb-8 text-2xl text-slate-500'>Improving productivity and certainty.</p>
         <Link href='/summary'>
-          <button className='bg-teal-600 text-white text-xl py-4 px-6 rounded hover:bg-teal-500'>Let's go</button>
+          <button className='px-6 py-4 text-xl text-white bg-teal-600 rounded hover:bg-teal-500'>Let's go</button>
         </Link>
       </div>
     </main>
