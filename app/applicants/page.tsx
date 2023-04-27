91e89c321fd0d1efd4cf457a2b203d8d1a73916c
Dave Reno <daveyreno86@gmail.com>
2023-04-27 12:59:31 +0800

    New Splash Pages

--- app/applicants/page.tsx
+++ app/applicants/page.tsx
@@ -4,7 +4,10 @@ export const metadata = {
 
 function ApplicantsPage() {
     return (
-        <div>Applicants Page</div>
+        <div className='flex flex-col items-center justify-center h-full'>
+            <h1 className='p-4 font-extrabold tracking-tighter text-transparent text-8xl bg-clip-text animate-gradient-x bg-gradient-to-r from-indigo-700 via-pink-600 to-blue-600'>Applicants</h1>
+            <p className='mt-2 text-2xl'>Nothing here yet boss.</p>
+        </div>
     )
 }
 
--- app/workshop-deal/page.tsx
+++ app/workshop-deal/page.tsx
@@ -4,7 +4,10 @@ export const metadata = {
 
 function WorkshopDealPage() {
     return (
-        <div>Workshop Your Deal Page</div>
+        <div className='flex flex-col items-center justify-center h-full'>
+            <h1 className='p-4 font-extrabold tracking-tighter text-transparent text-8xl bg-clip-text animate-gradient-x bg-gradient-to-r from-indigo-700 via-pink-600 to-blue-600'>Workshop Your Deal</h1>
+            <p className='mt-2 text-2xl'>Nothing here yet boss.</p>
+        </div>
     )
 }
 
